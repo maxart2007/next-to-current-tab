47729775c23fd97533676ed5b6c0013cc32bc531
Maxim Artyushenko <maxart2007@gmail.com>
2025-08-21 20:41:31 +0700

    feat: add options to control new tab placement

--- background.js
+++ background.js
@@ -1,15 +1,36 @@
-const lastActiveIndex = {};
+const lastActiveTab = {};
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
   chrome.tabs.get(activeInfo.tabId, (tab) => {
     if (chrome.runtime.lastError) return;
-    lastActiveIndex[tab.windowId] = tab.index;
+    lastActiveTab[tab.windowId] = { index: tab.index, groupId: tab.groupId };
   });
 });
 
 chrome.tabs.onCreated.addListener((tab) => {
-  const index = lastActiveIndex[tab.windowId];
-  if (typeof index === 'number') {
-    chrome.tabs.move(tab.id, { index: index + 1 });
-  }
+  chrome.storage.sync.get({ position: 'after', avoidGroups: false }, (prefs) => {
+    const last = lastActiveTab[tab.windowId];
+
+    if (prefs.position === 'start') {
+      chrome.tabs.move(tab.id, { index: 0 });
+      return;
+    }
+
+    if (prefs.position === 'end') {
+      chrome.tabs.move(tab.id, { index: -1 });
+      return;
+    }
+
+    if (last && typeof last.index === 'number') {
+      if (prefs.avoidGroups && last.groupId >= 0) {
+        chrome.tabs.query({ groupId: last.groupId, windowId: tab.windowId }, (groupTabs) => {
+          const maxIndex = Math.max(...groupTabs.map((t) => t.index));
+          chrome.tabs.move(tab.id, { index: maxIndex + 1 });
+        });
+      } else {
+        chrome.tabs.move(tab.id, { index: last.index + 1 });
+      }
+    }
+  });
 });
+
